cbc71eae651c52d308639e6f1d988e583f3c9dce
Alejandro <alejo8a_2@hotmail.com>
2017-05-30 00:31:10 -0500

    listo el crud de casos

--- controllers/caseController.js
+++ controllers/caseController.js
@@ -70,49 +70,22 @@ module.exports = function(app) {
         });
     })
     //Se necesita id y role
-    app.put('/Cases/add_role', function(req, res) {
-        if(req.body.role && req.body.id){
-            Cases.findById(req.body.id, function(err, user) {
-                if (err) throw err;
-                var roles = user.role;
-                if(user){
-                    if(roles.indexOf(req.body.role) != -1){
-                        res.send('User already has role ' + req.body.role + '. Changes were not made.')
-                    }else{
-                        console.log(roles);
-                        roles.push(req.body.role);
-                        console.log(roles);
-                        Cases.findByIdAndUpdate(req.body.id, { role: roles }, function(err, up_user) {
-                            if (err) throw err;
-                            res.send('Description added to '+ up_user.username);
-                        });
-                    }
-                }else{
-                    res.send('User not found')
-                }
-            });
-        }else{
-            res.send('Id and role needed');
-        }
-
-    });
-
-    app.delete('/Cases/delete_user', function(req, res) {
-
-        Cases.findByIdAndRemove(req.body.id, function(err, document) {
-            if (err) throw err;
-            res.send('User ' + document.username + ' deletion success.');
+    app.post('/cases/:id/edit', function(req, res) {
+        Cases.findByIdAndUpdate(req.params.id, {
+            title: req.body.title,
+            description: req.body.descripcion
+        }, function(err, newCase){
+            if (err)
+                throw err
+            res.redirect('/cases')
         })
-        res.send('Invalid user id');
+    });
+
+    app.get('/cases/:id/delete', function(req, res) {
+        Cases.findByIdAndRemove(req.params.id, function(err, document) {
+            if (err) throw err;
+            res.redirect('/cases');
+        })
 
     });
-    // app.delete('/todo', function(req, res) {
-
-    //     Todos.findByIdAndRemove(req.body.id, function(err) {
-    //         if (err) throw err;
-    //         res.send('Success');
-    //     })
-
-    // });
-
 }
